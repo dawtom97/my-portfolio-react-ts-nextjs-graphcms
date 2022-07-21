f88652bc35f156910ce43202e24cd1bc49cde30e
Dawid <daw.tomczyk1997@gmail.com>
2022-07-21 23:46:56 +0200

    large design refactor

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,9 +13,6 @@ function MyApp({ Component, pageProps }: AppProps) {
     <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
       <GlobalStyles />
       <Component {...pageProps} />
-      {/* <button className='switcher' onClick={themeToggler}>
-        Theme
-      </button> */}
     </ThemeProvider>
   );
 }
