84617cf3e96149ff0d97ef1d5d84f8bbce45fa50
Dawid <daw.tomczyk1997@gmail.com>
2022-08-03 17:26:32 +0200

    not existing loader import fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
-import loader from '../src/loader';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
